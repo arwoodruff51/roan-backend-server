c5fd20520efc673aeb1619c62c155d25fc476ee1
arwoodruff51 <arwoodruff@gmail.com>
2025-05-12 12:04:51 -0500

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -2,9 +2,9 @@ import os
 import json
 from google.oauth2.credentials import Credentials
 
-def load_google_credentials():
+def load_credentials():
     creds_data = os.environ.get("GOOGLE_TOKEN") or os.environ.get("RAILWAY_TOKEN_JSON")
     if not creds_data:
-        raise Exception("Missing Google OAuth token data in environment variables.")
+        raise Exception("❌ No token found in environment variables.")
     creds_dict = json.loads(creds_data)
     return Credentials.from_authorized_user_info(info=creds_dict)
